d11afc46da292463212439513359cbdfde2ef6bf
Vitalii <77769355+Vitaly-72@users.noreply.github.com>
2025-09-20 09:47:09 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    serverComponentsExternalPackages: ['canvas'],
   },
   images: {
     domains: [],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      canvas: false,
+    };
+    return config;
   },
 }
 
