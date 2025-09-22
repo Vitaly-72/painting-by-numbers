d6f0d3d13a0dc8488ba5071fbaf54c1150cbab0c
Vitalii <77769355+Vitaly-72@users.noreply.github.com>
2025-09-22 14:19:14 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // УДАЛИТЬ experimental.appDir - он не нужен в Next.js 14!
+  // App Router теперь включен по умолчанию
 }
 
 module.exports = nextConfig
