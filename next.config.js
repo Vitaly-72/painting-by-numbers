4ed15f79588af92fbd74ec77332ce6f9aa531d3e
Vitalii <77769355+Vitaly-72@users.noreply.github.com>
2025-09-22 13:43:38 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Добавьте эту настройку:
-  distDir: '.next',
-  // Явно укажите директорию с app
-  experimental: {
-    appDir: true,
-  },
-  // Укажите корневую директорию
-  basePath: '',
-  // Для монорепозиториев, но может помочь
-  reactStrictMode: true,
 }
 
 module.exports = nextConfig
