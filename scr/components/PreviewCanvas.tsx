5546024855d48741dffd724f215d66c2fc061e5f
Vitalii <77769355+Vitaly-72@users.noreply.github.com>
2025-09-22 15:09:35 +0500

    Update PreviewCanvas.tsx

--- scr/components/PreviewCanvas.tsx
+++ scr/components/PreviewCanvas.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useRef } from 'react'
-import { ProcessedImage } from '@/types'
+import { ProcessedImage } from '../types'
 
 interface PreviewCanvasProps {
   processedImage: ProcessedImage
